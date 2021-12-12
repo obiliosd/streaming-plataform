6d438f31fbd8eb97c6359a0b53adf6aa462f30e5
obiliosd <obiliosd@gmail.com>
2021-12-12 14:55:01 +0100

    add index cassandra

--- cassandra/query/CREATE TABLE RAIDERS.sql
+++ cassandra/query/CREATE TABLE RAIDERS.sql
@@ -9,9 +9,18 @@ coordinates list<double>,
 prediction int,
 PRIMARY KEY ((country,region),identifier));
 
+Create index raiderprediction on sparkstreaming.raiders(prediction);
+
+
 INSERT INTO raiders (identifier, country,region, coordinatesTime ,coordinates,prediction) 
 VALUES (1, 'Spain', 'Barcelona', '2021-10-17T13:18:20.766Z', [2.1535177931411975,41.39341576682141], 5);
 
 select * from raiders;
+
+select * from raiders 
+where country = 'Spain' 
+and region = 'Barcelona'
+and prediction = 3
+;
 
 drop table raiders;
